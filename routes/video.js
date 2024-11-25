10eb0ff0098c00b0005c959e15cc029f6d553b9d
satywan kumar <satywankumar646@gmail.com>
2024-11-25 14:30:37 +0530

    demo

--- routes/video.js
+++ routes/video.js
@@ -20,7 +20,7 @@ Router.get('/own-video',checkAuth,async (req,res)=>{
         const token = req.headers.authorization.split(" ")[1]
         const user = await jwt.verify(token, 'sbs online classes 123')
         console.log(user)
-        const videos = await Video.find({user_id:user._id}).populate('user_id','channelName logoUrl')
+        const videos = await Video.find({user_id:user._id}).populate('user_id','channelName logoUrl subscribers')
         res.status(200).json({
             videos:videos
         })
